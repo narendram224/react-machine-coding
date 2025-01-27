72d098e48bdb1b98eb0acbf413c209ab7220f508
narendra49 <narendra.maurya@leapfinance.com>
2025-01-27 13:01:36 +0530

    [React router 7]: installed and set path for folder component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import "./App.css";
 import Folder from "./components/Folder/Folder";
 import { folderConfig } from "./components/Folder/folder.config";
 import styles from "./components/Folder/Folder.module.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.folderContainer}>
-      <Folder config={folderConfig} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Folder config={folderConfig} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
