cfda792fd2ab32bc2f28d12c7c7032d5a135a491
narendra49 <narendra.maurya@leapfinance.com>
2025-01-27 13:41:44 +0530

    Routing setup in react js

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import ComponentPath from "./ComponentPath";
 import Folder from "./components/Folder/Folder";
 import { folderConfig } from "./components/Folder/folder.config";
 import styles from "./components/Folder/Folder.module.css";
@@ -8,10 +7,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className={styles.folderContainer}>
+    <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Folder config={folderConfig} />} />
+          <Route path="/" element={<ComponentPath />} />
+          <Route
+            path="/folder"
+            element={
+              <div className={styles.folderContainer}>
+                <Folder config={folderConfig} className={styles.folder} />
+              </div>
+            }
+          />
         </Routes>
       </Router>
     </div>
---
+++ src/components/config.ts
@@ -0,0 +1,8 @@
+export const renderListConfig = [
+  {
+    link: "/folder",
+    label: "React Recursive Folder Component",
+    description: `Learn how to build a folder component with recusion and make it
+                  dynamic`,
+  },
+];
